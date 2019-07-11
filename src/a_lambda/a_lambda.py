3959c05dc31ec6588c4d6d8f56b41d6cbb9b6109
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-07-11 10:39:12 +0100

    redone

--- src/a_lambda/a_lambda.py
+++ src/a_lambda/a_lambda.py
@@ -1,8 +1,8 @@
-from common import hello_func
+from .common import hello_func
 
 
 def lambda_handler(event, context):  # pylint:disable=unused-argument
     return {
         "statusCode": 200,
-        "body": hello_func("a_lambda")
+        "body": hello_func("b_lambda")
     }
