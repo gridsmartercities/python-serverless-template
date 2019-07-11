dd6728edf193d5e8912a3ee2ffe37399763304bd
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-07-11 11:06:47 +0100

    added external package to test dependencies

--- src/a_lambda/a_lambda.py
+++ src/a_lambda/a_lambda.py
@@ -1,6 +1,8 @@
 from src.common import hello_func
+from aws_lambda_decorators import cors
 
 
+@cors()
 def lambda_handler(event, context):  # pylint:disable=unused-argument
     return {
         "statusCode": 200,
