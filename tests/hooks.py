4a39c55fe3f117fbfe3b51f13aa124450fa4853a
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-07-11 14:25:59 +0100

    removed /Prod

--- tests/hooks.py
+++ tests/hooks.py
@@ -6,11 +6,11 @@ def un_skip(transaction):
     transaction["skip"] = False
 
 
-@hooks.before("/Prod/alambda > GET > 500 > application/json")
+@hooks.before("/alambda > GET > 500 > application/json")
 def before_alambda_put_500(transaction):
     transaction["skip"] = True
 
 
-@hooks.before("/Prod/blambda > GET > 500 > application/json")
+@hooks.before("/blambda > GET > 500 > application/json")
 def before_blambda_put_500(transaction):
     transaction["skip"] = True
