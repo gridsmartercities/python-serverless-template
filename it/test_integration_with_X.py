b752f2c018db5afbf8ec3ac6c3d6429a4babb1c3
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-07-15 12:00:36 +0100

    integration tests added, deleted __init__:

---
+++ it/test_integration_with_X.py
@@ -0,0 +1,10 @@
+import unittest
+from src.a_lambda.a_lambda import lambda_handler
+
+
+class IntegrationTests(unittest.TestCase):
+
+    def test_success(self):
+        response = lambda_handler(None, None)
+        self.assertEqual(200, response["statusCode"])
+        self.assertEqual("""{"message": "HELLO FROM A_LAMBDA"}""", response["body"])
