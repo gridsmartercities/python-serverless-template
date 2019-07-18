e201cf1ab3d8a3dafed15742be2f1dc09e9ad49f
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-07-18 14:45:38 +0100

    naming

--- tests/b_lambda/test_b_lambda.py
+++ tests/b_lambda/test_b_lambda.py
@@ -1,10 +1,10 @@
 import unittest
-from src.b_lambda.b_lambda import lambda_handler
+from src.b_lambda.b_lambda import handler
 
 
 class BLambdaTests(unittest.TestCase):
 
     def test_success(self):
-        response = lambda_handler(None, None)
+        response = handler(None, None)
         self.assertEqual(200, response["statusCode"])
         self.assertEqual("""{"message": "HELLO FROM B_LAMBDA"}""", response["body"])
