68691354bedc324f4c930be5bbc07a5d8b4be2e0
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-07-19 08:41:51 +0100

    renaming

--- it/test_integration_with_x.py
+++ it/test_integration_with_x.py
@@ -5,6 +5,10 @@ from src.a_lambda.a_lambda import handler
 class IntegrationTests(unittest.TestCase):
 
     def test_success(self):
+        # ...
+
         response = handler(None, None)
         self.assertEqual(200, response["statusCode"])
         self.assertEqual("""{"message": "HELLO FROM A_LAMBDA"}""", response["body"])
+
+        # ...
