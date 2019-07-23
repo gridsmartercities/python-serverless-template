d737343d37baa4d2a50e46b61a961da8904c842c
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-07-23 14:03:18 +0100

    fixed tests

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,10 +1,9 @@
 import unittest
-# from src.common import hello_func
+from src.common import hello_func
 
 
 class CommonTests(unittest.TestCase):
 
     def test_success(self):
-        # response = hello_func("TEST")
-        # self.assertEqual({"message": "HELLO FROM TEST"}, response)
-        self.assertEqual(1, 1)
+        response = hello_func("TEST")
+        self.assertEqual({"message": "HELLO FROM TEST"}, response)
