ac940550833a11859b972caae56cc0424ccc6c7a
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-07-29 11:42:59 +0100

    permissions

--- tools/build/hub-installer.sh
+++ tools/build/hub-installer.sh
@@ -4,5 +4,6 @@ set -e
 wget https://github.com/github/hub/releases/download/v2.12.3/hub-linux-amd64-2.12.3.tgz
 tar -xvzf hub-linux-amd64-2.12.3.tgz
 mv hub-linux-amd64-2.12.3 /opt/tools/hub
+chmod u+x -R /opt/tools/hub
 export PATH="/opt/tools/hub/bin:$PATH"
 hub --version
