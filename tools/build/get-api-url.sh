05714503cb8a4e38011fb8963d93491b71344f35
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-07-30 11:15:59 +0100

    added get-api-url script

---
+++ tools/build/get-api-url.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+function show_help() {
+    echo -e "\nUSAGE:\n\t./get-api-url.sh <API_NAME>"
+}
+
+if [[ $# -eq 0 ]]; then
+    echo "No arguments supplied"
+    show_help
+    exit 1
+fi
+
+api_name=$1
+
+api_id=`aws apigateway get-rest-apis --query "items[?name=='$1'].id" | jq first | tr -d '"'`
+echo https://$api_id.execute-api.$AWS_REGION.amazonaws.com/Prod
+
+exit 0
