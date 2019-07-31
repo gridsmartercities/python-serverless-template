a4a1b56be83090b113e4e8edbe8f977670b3efea
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-07-31 10:49:39 +0100

    all except create commit status done

--- tools/build/pre-build-checks.sh
+++ tools/build/pre-build-checks.sh
@@ -1,47 +1,51 @@
 #!/bin/bash
 
+function show_failure() {
+    echo -e "\n... FAILED!\n"
+    exit 1
+}
 
 echo "... validating sam template"
 if [[ ! $(aws cloudformation validate-template --template-body file://template.yaml) ]]; then
-    exit 1
+    show_failure
 fi
 
 echo "... checking sam template"
 cfn-lint template.yaml
 if [[ $? != 0 ]]; then
-    exit 1
+    show_failure
 fi
 
 echo "... validating OpenApi specification"
 if [[ ! $(swagger-cli validate api-contract.yaml) ]]; then
-    exit 1
+    show_failure
 fi
 
 echo "... running prospector tools"
 if ! prospector; then
-    exit 1
+    show_failure
 fi
 
 echo "... running security tests"
 bandit -r -q .
 if [[ $? != 0 ]]; then
-    exit 1
+    show_failure
 fi
 
 echo "... running unit tests"
 coverage run --branch --source='.' -m unittest tests/test_*.py
 if [[ $? != 0 ]]; then
-    exit 1
+    show_failure
 fi
 coverage run --branch --source='.' -m unittest tests/*/test_*.py
 if [[ $? != 0 ]]; then
-    exit 1
+    show_failure
 fi
 
 echo "... checking coverage"
 coverage report -m --fail-under=100 --omit=tests/*,it/*
 if [[ $? != 0 ]]; then
-    exit 1
+    show_failure
 fi
 
 echo -e "\n... SUCCESS!\n"
--- tools/build/report-status.sh
+++ tools/build/report-status.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 function show_help() {
     echo -e "\nUSAGE:\n\t./report-status.sh <REPO_NAME> <COMMIT_SHA> <CONTEXT> <COMMAND>"
@@ -11,7 +10,26 @@ if [[ $# -ne 4 ]]; then
     exit 0
 fi
 
-api_id=`aws apigateway get-rest-apis --query "items[?name=='$1'].id" | jq first | tr -d '"'`
-echo https://$api_id.execute-api.$AWS_REGION.amazonaws.com/Prod
+REPO_NAME=$1
+COMMIT_SHA=$2
+CONTEXT=$3
+COMMAND=$4
+
+function create_commit_status() {
+    echo "creating commit status $1 $2 $3 $4"
+}
+
+create_commit_status $COMMIT_SHA "pending" "job starting" $CONTEXT
+
+STATE="success"
+DESCRIPTION="job succeeded"
+
+$COMMAND
+if [[ $? -ne 0 ]]; then
+    STATE="failure"
+    DESCRIPTION="job failed"
+fi
+
+create_commit_status $COMMIT_SHA $STATE $DESCRIPTION $CONTEXT
 
 exit 0
