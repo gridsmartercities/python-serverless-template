fa25679046cb08c65576d2fe8491b2fcde432df9
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-08-01 07:40:03 +0100

    removed test

--- it/test_integration_with_x.py
+++ it/test_integration_with_x.py
@@ -12,12 +12,3 @@ class IntegrationTests(unittest.TestCase):
         self.assertEqual("""{"message": "HELLO FROM A_LAMBDA"}""", response["body"])
 
         # ...
-
-    def test_success2(self):
-        # ...
-
-        response = handler(None, None)
-        self.assertEqual(200, response["statusCode"])
-        self.assertEqual("""{"message": "HELLO FROM A_LAMBDA"}""", response["body"])
-
-        # ...
