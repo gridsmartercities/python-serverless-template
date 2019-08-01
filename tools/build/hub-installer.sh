237cd086479de0d7cb3a720d9c561649f05bab79
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-08-01 07:26:34 +0100

    path

--- tools/build/hub-installer.sh
+++ tools/build/hub-installer.sh
@@ -5,4 +5,4 @@ wget https://github.com/github/hub/releases/download/v2.12.3/hub-linux-amd64-2.1
 tar -xzf hub-linux-amd64-2.12.3.tgz
 mv hub-linux-amd64-2.12.3 /opt/tools/hub
 
-export PATH="$PATH:/opt/tools/hub/bin"
+export PATH=$PATH:/opt/tools/hub/bin
