248459ce8b75242b492b7bf1279d6a338f2450dc
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-08-05 08:31:32 +0100

    removed pip installation of aws-sam-cli

---
+++ tools/build/aws-sam-cli-installer.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
