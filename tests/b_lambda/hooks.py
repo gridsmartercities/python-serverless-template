de9733baf622efead039fc507e7348f53532866a
Eulogio Gutierrez <43067518+eulogio-gutierrez@users.noreply.github.com>
2019-08-06 14:11:11 +0100

    Update hooks.py

--- tests/b_lambda/hooks.py
+++ tests/b_lambda/hooks.py
@@ -2,5 +2,5 @@ import dredd_hooks as hooks
 
 
 @hooks.before("/blambda > GET > 500 > application/json")
-def before_blambda_get_500(transaction):
+def before_b_lambda_get_500(transaction):
     transaction["skip"] = True
