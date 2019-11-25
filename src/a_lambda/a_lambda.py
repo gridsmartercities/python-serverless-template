d9e42e518cc6259105a225a7a5b49db6b08a1c20
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-11-25 14:15:32 +0000

    cosmetic changes

--- src/a_lambda/a_lambda.py
+++ src/a_lambda/a_lambda.py
@@ -1,11 +1,16 @@
 from aws_lambda_decorators import cors, log, response_body_as_json
 from src.common import hello_func
+from src.logger import get_logger
+
+
+LOGGER = get_logger(__name__)
 
 
 @cors()
 @log(parameters=True, response=True)
 @response_body_as_json
 def handler(event, context):  # pylint:disable=unused-argument
+    LOGGER.info("Some info message")
     return {
         "statusCode": 200,
         "body": hello_func("a_lambda")
