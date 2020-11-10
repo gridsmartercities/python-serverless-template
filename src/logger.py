4e6afece590a1d5bedadf687e2a9da511f11c1a4
Dan Pudwell <dan.pudwell@gridsmartercities.com>
2020-11-10 16:04:31 +0000

    fixed mypy

--- src/logger.py
+++ src/logger.py
@@ -1,11 +1,12 @@
 import logging
+from logging import Logger
 import os
 
 
 LOG_LEVEL = getattr(logging, os.getenv("LOG_LEVEL", "INFO"))
 
 
-def get_logger(name):
+def get_logger(name: str) -> Logger:
     logger = logging.getLogger(name)
     logger.setLevel(LOG_LEVEL)
     return logger
